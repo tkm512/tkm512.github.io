4f7025fed6b8f123fb40b7c5ce1bd24d871d4af5
tkm512 <67851164+tkm512@users.noreply.github.com>
2020-07-12 21:48:53 +0900

    third
    passengers update

---
+++ passengers/mychart.js
@@ -0,0 +1,48 @@
+// 2) CSVから２次元配列に変換
+function csv2Array(str) {
+  var csvData = [];
+  var lines = str.split("\n");
+  for (var i = 0; i < lines.length; ++i) {
+    var cells = lines[i].split(",");
+    csvData.push(cells);
+  }
+  return csvData;
+}
+
+function drawBarChart(data) {
+  // 3)chart.jsのdataset用の配列を用意
+  var tmpLabel1 = [], tmpLabel2 = [], tmpData1 = [];
+  for (var row in data) {
+    tmpLabel1.push(data[row][0])
+    tmpLabel2.push(data[row][1])
+    tmpData1.push(data[row][2])
+  };
+
+  // 4)chart.jsで描画
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: tmpLabel1,
+      datasets: [
+        { label: "乗車人員", data: tmpData1, backgroundColor: "blue" }
+      ]
+    }
+  });
+}
+
+function main() {
+  // 1) ajaxでCSVファイルをロード
+  var req = new XMLHttpRequest();
+  var filePath = './csv/r1jrhsapporocity_sort.csv';
+  req.open("GET", filePath, true);
+  req.onload = function() {
+    // 2) CSVデータ変換の呼び出し
+    data = csv2Array(req.responseText);
+    // 3) chart.jsデータ準備、4) chart.js描画の呼び出し
+    drawBarChart(data);
+  }
+  req.send(null);
+}
+
+main();
